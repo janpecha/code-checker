5e2c10828056a32b5112c07f515afe3892f02032
Jan Pecha <janpecha@email.cz>
2022-12-20 13:37:11 +0100

    Sets: added CzProjectMinimum

---
+++ src/Sets/CzProjectMinimum.php
@@ -0,0 +1,16 @@
+<?php
+
+	declare(strict_types=1);
+
+	namespace JP\CodeChecker\Sets;
+
+	use JP\CodeChecker\CheckerConfig;
+
+
+	class CzProjectMinimum
+	{
+		public static function configure(CheckerConfig $config): void
+		{
+			\JP\CodeChecker\Tasks\AutoConfig::configure($config);
+		}
+	}
